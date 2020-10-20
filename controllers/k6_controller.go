3ab20569f855188bc0e8f43acc9dcfc0b46226e1
Simon Aronsson <simme@arcticbit.se>
2020-10-20 15:37:38 +0200

    clean up

--- controllers/k6_controller.go
+++ controllers/k6_controller.go
@@ -37,22 +37,19 @@ type K6Reconciler struct {
 	Scheme *runtime.Scheme
 }
 
-// +kubebuilder:rbac:groups=k6.io,resources=k6s,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=k6.io,resources=k6s/status,verbs=get;update;patch
-
+// Reconcile takes a K6 object and takes the appropriate action in the cluster
 // +kubebuilder:rbac:groups=k6.io,resources=k6s,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=k6.io,resources=k6s/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;
 // +kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;watch;create;update;patch;delete
-
 func (r *K6Reconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	ctx := context.Background()
 	log := r.Log.WithValues("k6", req.NamespacedName)
 
 	result := ctrl.Result{}
 
-	// Fetch the applied CRD
+	// Fetch the CRD
 	k6 := &v1alpha1.K6{}
 	err := r.Get(ctx, req.NamespacedName, k6)
 	if err != nil {
@@ -66,27 +63,43 @@ func (r *K6Reconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 
 	// Check for previous jobs
 	found := &batchv1.Job{}
-	err = r.Get(ctx, types.NamespacedName{Name: fmt.Sprintf("%s-1", k6.Name), Namespace: k6.Namespace}, found)
-	if err == nil || !errors.IsNotFound(err) {
+	namespacedName := types.NamespacedName{
+		Name:      fmt.Sprintf("%s-1", k6.Name),
+		Namespace: k6.Namespace,
+	}
+
+	if err = r.Get(ctx, namespacedName, found); err == nil || !errors.IsNotFound(err) {
 		log.Info("Could not start a new test, Make sure you've deleted your previous run.")
 		return result, err
 	}
 
 	// Create jobs
 	for i := 1; i <= int(k6.Spec.Parallelism); i++ {
-		log.Info(fmt.Sprintf("Launching k6 test #%d", i))
-		job := resources.NewJob(k6, i)
-		if err = ctrl.SetControllerReference(k6, job, r.Scheme); err != nil {
-			log.Error(err, "Failed to set controller reference for job")
-			return ctrl.Result{}, err
-		}
-		if err = r.Create(ctx, job); err != nil {
-			log.Error(err, "Failed to launch k6 test")
+		if err = r.LaunchTest(ctx, k6, i, log); err != nil {
 			return ctrl.Result{}, err
 		}
 	}
 
 	return result, nil
+}
+
+func (r *K6Reconciler) LaunchTest(ctx context.Context, k6 *v1alpha1.K6, index int, log logr.Logger) error {
+	msg := fmt.Sprintf("Launching k6 test #%d", index)
+	log.Info(msg)
+
+	job := resources.NewJob(k6, index)
+
+	if err := ctrl.SetControllerReference(k6, job, r.Scheme); err != nil {
+		log.Error(err, "Failed to set controller reference for job")
+		return err
+	}
+
+	if err := r.Create(ctx, job); err != nil {
+		log.Error(err, "Failed to launch k6 test")
+		return err
+	}
+
+	return nil
 }
 
 func (r *K6Reconciler) SetupWithManager(mgr ctrl.Manager) error {
