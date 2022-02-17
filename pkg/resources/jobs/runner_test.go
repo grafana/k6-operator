d702616d5929f3407a69143952af5987dbd6da2a
Adam D'Abbracci <adam.dabbracci@nytimes.com>
2022-02-17 14:51:00 -0500

    Recreated fix from correct branch

--- pkg/resources/jobs/runner_test.go
+++ pkg/resources/jobs/runner_test.go
@@ -16,7 +16,7 @@ func TestNewScriptVolumeClaim(t *testing.T) {
 
 	expectedOutcome := &Script{
 		Name: "Test",
-		File: "/test/thing.js",
+		File: "thing.js",
 		Type: "VolumeClaim",
 	}
 
@@ -42,7 +42,7 @@ func TestNewScriptConfigMap(t *testing.T) {
 
 	expectedOutcome := &Script{
 		Name: "Test",
-		File: "/test/thing.js",
+		File: "thing.js",
 		Type: "ConfigMap",
 	}
 
@@ -64,41 +64,17 @@ func TestNewScriptConfigMap(t *testing.T) {
 	}
 }
 
-func TestNewScriptLocalFile(t *testing.T) {
-
-	expectedOutcome := &Script{
-		Name: "LocalFile",
-		File: "/custom/my_test.js",
-		Type: "LocalFile",
-	}
-
-	k6 := v1alpha1.K6Spec{
-		Script: v1alpha1.K6Script{
-			LocalFile: "/custom/my_test.js",
-		},
-	}
-
-	script, err := newScript(k6)
-	if err != nil {
-		t.Errorf("NewScript with LocalFile errored, got: %v, want: %v", err, expectedOutcome)
-	}
-	if !reflect.DeepEqual(script, expectedOutcome) {
-		t.Errorf("NewScript with LocalFile failed to return expected output, got: %v, expected: %v", script, expectedOutcome)
-	}
-}
-
 func TestNewScriptNoScript(t *testing.T) {
 
 	k6 := v1alpha1.K6Spec{}
 
 	script, err := newScript(k6)
 	if err == nil && script != nil {
-		t.Errorf("Expected Error from NewScript, got: %v, want: %v", err, errors.New("configMap, VolumeClaim or LocalFile not provided in script definition"))
+		t.Errorf("Expected Error from NewScript, got: %v, want: %v", err, errors.New("configMap or VolumeClaim not provided in script definition"))
 	}
 }
 
 func TestNewVolumeSpecVolumeClaim(t *testing.T) {
-
 	expectedOutcome := []corev1.Volume{{
 		Name: "k6-test-volume",
 		VolumeSource: corev1.VolumeSource{
@@ -143,7 +119,16 @@ func TestNewVolumeSpecConfigMap(t *testing.T) {
 }
 
 func TestNewVolumeSpecNoType(t *testing.T) {
-	expectedOutcome := []corev1.Volume{}
+	expectedOutcome := []corev1.Volume{{
+		Name: "k6-test-volume",
+		VolumeSource: corev1.VolumeSource{
+			ConfigMap: &corev1.ConfigMapVolumeSource{
+				LocalObjectReference: corev1.LocalObjectReference{
+					Name: "test",
+				},
+			},
+		},
+	}}
 
 	k6 := &Script{
 		Name: "test",
@@ -371,11 +356,11 @@ func TestNewRunnerJobNoisy(t *testing.T) {
 				Spec: corev1.PodSpec{
 					Hostname:                     "test-1",
 					RestartPolicy:                corev1.RestartPolicyNever,
+					SecurityContext:              &corev1.PodSecurityContext{},
 					Affinity:                     nil,
 					NodeSelector:                 nil,
 					ServiceAccountName:           "default",
 					AutomountServiceAccountToken: &automountServiceAccountToken,
-					SecurityContext:              &corev1.PodSecurityContext{},
 					Containers: []corev1.Container{{
 						Image:     "ghcr.io/grafana/operator:latest-runner",
 						Name:      "k6",
@@ -467,11 +452,11 @@ func TestNewRunnerJobUnpaused(t *testing.T) {
 				Spec: corev1.PodSpec{
 					Hostname:                     "test-1",
 					RestartPolicy:                corev1.RestartPolicyNever,
+					SecurityContext:              &corev1.PodSecurityContext{},
 					Affinity:                     nil,
 					NodeSelector:                 nil,
 					ServiceAccountName:           "default",
 					AutomountServiceAccountToken: &automountServiceAccountToken,
-					SecurityContext:              &corev1.PodSecurityContext{},
 					Containers: []corev1.Container{{
 						Image:     "ghcr.io/grafana/operator:latest-runner",
 						Name:      "k6",
@@ -563,11 +548,11 @@ func TestNewRunnerJobArguments(t *testing.T) {
 				Spec: corev1.PodSpec{
 					Hostname:                     "test-1",
 					RestartPolicy:                corev1.RestartPolicyNever,
+					SecurityContext:              &corev1.PodSecurityContext{},
 					Affinity:                     nil,
 					NodeSelector:                 nil,
 					ServiceAccountName:           "default",
 					AutomountServiceAccountToken: &automountServiceAccountToken,
-					SecurityContext:              &corev1.PodSecurityContext{},
 					Containers: []corev1.Container{{
 						Image:     "ghcr.io/grafana/operator:latest-runner",
 						Name:      "k6",
@@ -660,11 +645,11 @@ func TestNewRunnerJobServiceAccount(t *testing.T) {
 				Spec: corev1.PodSpec{
 					Hostname:                     "test-1",
 					RestartPolicy:                corev1.RestartPolicyNever,
+					SecurityContext:              &corev1.PodSecurityContext{},
 					Affinity:                     nil,
 					NodeSelector:                 nil,
 					ServiceAccountName:           "test",
 					AutomountServiceAccountToken: &automountServiceAccountToken,
-					SecurityContext:              &corev1.PodSecurityContext{},
 					Containers: []corev1.Container{{
 						Image:     "ghcr.io/grafana/operator:latest-runner",
 						Name:      "k6",
@@ -758,11 +743,11 @@ func TestNewRunnerJobIstio(t *testing.T) {
 				Spec: corev1.PodSpec{
 					Hostname:                     "test-1",
 					RestartPolicy:                corev1.RestartPolicyNever,
+					SecurityContext:              &corev1.PodSecurityContext{},
 					Affinity:                     nil,
 					NodeSelector:                 nil,
 					ServiceAccountName:           "default",
 					AutomountServiceAccountToken: &automountServiceAccountToken,
-					SecurityContext:              &corev1.PodSecurityContext{},
 					Containers: []corev1.Container{{
 						Image:   "ghcr.io/grafana/operator:latest-runner",
 						Name:    "k6",
@@ -808,98 +793,6 @@ func TestNewRunnerJobIstio(t *testing.T) {
 					Name: "test",
 					File: "test.js",
 				},
-			},
-			Runner: v1alpha1.Pod{
-				Metadata: v1alpha1.PodMetadata{
-					Labels: map[string]string{
-						"label1": "awesome",
-					},
-					Annotations: map[string]string{
-						"awesomeAnnotation": "dope",
-					},
-				},
-			},
-		},
-	}
-
-	job, err := NewRunnerJob(k6, 1)
-	if err != nil {
-		t.Errorf("NewRunnerJob errored, got: %v", err)
-	}
-	if diff := deep.Equal(job, expectedOutcome); diff != nil {
-		t.Errorf("NewRunnerJob returned unexpected data, diff: %s", diff)
-	}
-}
-
-func TestNewRunnerJobLocalFile(t *testing.T) {
-	script := &Script{
-		Name: "test",
-		File: "/test/test.js",
-		Type: "LocalFile",
-	}
-
-	var zero int64 = 0
-	automountServiceAccountToken := true
-
-	expectedOutcome := &batchv1.Job{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "test-1",
-			Namespace: "test",
-			Labels: map[string]string{
-				"app":    "k6",
-				"k6_cr":  "test",
-				"label1": "awesome",
-			},
-			Annotations: map[string]string{
-				"awesomeAnnotation": "dope",
-			},
-		},
-		Spec: batchv1.JobSpec{
-			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: map[string]string{
-						"app":    "k6",
-						"k6_cr":  "test",
-						"label1": "awesome",
-					},
-					Annotations: map[string]string{
-						"awesomeAnnotation": "dope",
-					},
-				},
-				Spec: corev1.PodSpec{
-					Hostname:                     "test-1",
-					RestartPolicy:                corev1.RestartPolicyNever,
-					Affinity:                     nil,
-					NodeSelector:                 nil,
-					ServiceAccountName:           "default",
-					AutomountServiceAccountToken: &automountServiceAccountToken,
-					SecurityContext:              &corev1.PodSecurityContext{},
-					Containers: []corev1.Container{{
-						Image:        "ghcr.io/grafana/operator:latest-runner",
-						Name:         "k6",
-						Command:      []string{"sh", "-c", "if [ ! -f /test/test.js ]; then echo \"LocalFile not found exiting...\"; exit 1; fi;\nk6 run --quiet /test/test.js --address=0.0.0.0:6565 --paused"},
-						Env:          []corev1.EnvVar{},
-						Resources:    corev1.ResourceRequirements{},
-						VolumeMounts: []corev1.VolumeMount{},
-						Ports:        []corev1.ContainerPort{{ContainerPort: 6565}},
-					}},
-					TerminationGracePeriodSeconds: &zero,
-					Volumes:                       newVolumeSpec(script),
-				},
-			},
-		},
-	}
-	k6 := &v1alpha1.K6{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "test",
-			Namespace: "test",
-		},
-		Spec: v1alpha1.K6Spec{
-			Scuttle: v1alpha1.K6Scuttle{
-				Enabled: "false",
-			},
-			Script: v1alpha1.K6Script{
-				LocalFile: "/test/test.js",
 			},
 			Runner: v1alpha1.Pod{
 				Metadata: v1alpha1.PodMetadata{
