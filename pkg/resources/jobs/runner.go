02dfbc990b8c2b3e788f5c8fd8bd471a9d6be0e3
spoukke <32708678+spoukke@users.noreply.github.com>
2022-09-01 15:08:19 +0100

    feat: use name instead of index for instance id tag (#135)
    * feat: use name instead of index for instance id tag
    
    * feat: add tag for job_name instead of changing instance_id
    
    * test: add the new tag to unit tests

--- pkg/resources/jobs/runner.go
+++ pkg/resources/jobs/runner.go
@@ -68,6 +68,9 @@ func NewRunnerJob(k6 *v1alpha1.K6, index int, testRunId, token string) (*batchv1
 	// Add an instance tag: in case metrics are stored, they need to be distinguished by instance
 	command = append(command, "--tag", fmt.Sprintf("instance_id=%d", index))
 
+	// Add an job tag: in case metrics are stored, they need to be distinguished by job
+	command = append(command, "--tag", fmt.Sprintf("job_name=%s", name))
+
 	command = script.UpdateCommand(command)
 
 	var (
