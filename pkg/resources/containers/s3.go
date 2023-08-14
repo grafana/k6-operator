70c7d76370d4a312a856bc783607b5a0941f2147
Olha Yevtushenko <yorugac@gmail.com>
2023-08-14 18:47:29 +0300

    plz: fix command of init container

--- pkg/resources/containers/s3.go
+++ pkg/resources/containers/s3.go
@@ -12,7 +12,7 @@ func NewS3InitContainer(uri, image string, volumeMount corev1.VolumeMount) v1alp
 	return v1alpha1.InitContainer{
 		Name:         "archive-download",
 		Image:        image,
-		Command:      []string{fmt.Sprintf("curl -X GET -L '%s' > /test/archive.tar ; ls -l /test", uri)},
+		Command:      []string{"sh", "-c", fmt.Sprintf("curl -X GET -L '%s' > /test/archive.tar ; ls -l /test", uri)},
 		VolumeMounts: []corev1.VolumeMount{volumeMount},
 	}
 }
