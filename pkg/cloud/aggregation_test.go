f7388413a9009d03d21a259efd11bbcb939627ad
Olha Yevtushenko <yorugac@gmail.com>
2024-07-05 17:38:42 +0300

    cloud: remove decoding for cloud output v1

--- pkg/cloud/aggregation_test.go
+++ pkg/cloud/aggregation_test.go
@@ -30,38 +30,9 @@ func Test_EncodeAggregationConfig(t *testing.T) {
 
 func Test_DecodeAggregationConfig(t *testing.T) {
 	var (
-		// For now, we support both versions in decoding.
-		v1Encoded = "50|3s|8s|6s|10000|10"
-		v2Encoded = "2|5s|3s|10s|10"
+		encoded = "2|5s|3s|10s|10"
 
-		v1EnvVars = []corev1.EnvVar{
-			{
-				Name:  "K6_CLOUD_AGGREGATION_MIN_SAMPLES",
-				Value: "50",
-			},
-			{
-				Name:  "K6_CLOUD_AGGREGATION_PERIOD",
-				Value: "3s",
-			},
-			{
-				Name:  "K6_CLOUD_AGGREGATION_WAIT_PERIOD",
-				Value: "8s",
-			},
-			{
-				Name:  "K6_CLOUD_METRIC_PUSH_INTERVAL",
-				Value: "6s",
-			},
-			{
-				Name:  "K6_CLOUD_MAX_METRIC_SAMPLES_PER_PACKAGE",
-				Value: "10000",
-			},
-			{
-				Name:  "K6_CLOUD_MAX_METRIC_PUSH_CONCURRENCY",
-				Value: "10",
-			},
-		}
-
-		v2EnvVars = []corev1.EnvVar{
+		expected = []corev1.EnvVar{
 			{
 				Name:  "K6_CLOUD_API_VERSION",
 				Value: "2",
@@ -85,16 +56,10 @@ func Test_DecodeAggregationConfig(t *testing.T) {
 		}
 	)
 
-	envVars, err := DecodeAggregationConfig(v1Encoded)
+	envVars, err := DecodeAggregationConfig(encoded)
 	assert.Equal(t, nil, err)
 
-	for i, expectedEnvVar := range v1EnvVars {
-		assert.Equal(t, expectedEnvVar, envVars[i])
-	}
-
-	envVars, err = DecodeAggregationConfig(v2Encoded)
-	assert.Equal(t, nil, err)
-	for i, expectedEnvVar := range v2EnvVars {
+	for i, expectedEnvVar := range expected {
 		assert.Equal(t, expectedEnvVar, envVars[i])
 	}
 }
