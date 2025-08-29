b054a8d598aa2a2aa0d59f327d1d04102a0e049e
Sonu Kumar Meena <someena@thousandeyes.com>
2025-08-29 20:48:34 +0100

    Added feature to interpolate env vars in command args

--- go.mod
+++ go.mod
@@ -19,7 +19,9 @@ require (
 )
 
 require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/buildkite/interpolate v0.1.5 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
@@ -35,6 +37,7 @@ require (
 	github.com/go-openapi/swag v0.23.1 // indirect
 	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/mock v1.7.0-rc.1 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/btree v1.1.3 // indirect
 	github.com/google/gnostic-models v0.6.9 // indirect
@@ -54,6 +57,7 @@ require (
 	github.com/mstoykov/atlas v0.0.0-20220811071828-388f114305dd // indirect
 	github.com/mstoykov/envconfig v1.5.0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/prometheus/client_golang v1.21.1 // indirect
@@ -63,6 +67,9 @@ require (
 	github.com/redis/go-redis/v9 v9.5.5 // indirect
 	github.com/spf13/afero v1.14.0 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
+	github.com/twmb/murmur3 v1.1.8 // indirect
+	github.com/uber-go/tally v3.5.8+incompatible // indirect
+	github.com/uber/tchannel-go v1.34.4 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/otel v1.35.0 // indirect
@@ -73,8 +80,15 @@ require (
 	go.opentelemetry.io/otel/sdk v1.35.0 // indirect
 	go.opentelemetry.io/otel/trace v1.35.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.5.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/net/metrics v1.4.0 // indirect
+	go.uber.org/thriftrw v1.32.0 // indirect
+	go.uber.org/yarpc v1.80.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/exp/typeparams v0.0.0-20221208152030-732eee02a75a // indirect
+	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
+	golang.org/x/mod v0.24.0 // indirect
 	golang.org/x/net v0.39.0 // indirect
 	golang.org/x/oauth2 v0.28.0 // indirect
 	golang.org/x/sync v0.13.0 // indirect
@@ -91,6 +105,7 @@ require (
 	gopkg.in/evanphx/json-patch.v4 v4.12.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	honnef.co/go/tools v0.4.3 // indirect
 	k8s.io/apiextensions-apiserver v0.32.3 // indirect
 	k8s.io/klog/v2 v2.130.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20250318142150-58bc1449e42a // indirect
--- pkg/resources/jobs/runner.go
+++ pkg/resources/jobs/runner.go
@@ -2,6 +2,7 @@ package jobs
 
 import (
 	"fmt"
+	"github.com/buildkite/interpolate"
 	"strconv"
 	"strings"
 
@@ -48,8 +49,50 @@ func NewRunnerJob(k6 *v1alpha1.TestRun, index int, tokenInfo *cloud.TokenInfo) (
 		return nil, err
 	}
 
+	// Conslidate envs
+	env := newIstioEnvVar(k6.GetSpec().Scuttle, istioEnabled)
+
+	// this is a cloud test run
+	if len(k6.TestRunID()) > 0 {
+		// cloud output case
+		tokenVar := corev1.EnvVar{
+			Name:  "K6_CLOUD_TOKEN",
+			Value: tokenInfo.Value(),
+		}
+
+		if v1alpha1.IsTrue(k6, v1alpha1.CloudPLZTestRun) {
+			// temporary hack
+			k6.GetStatus().AggregationVars = "2|5s|3s|10s|10"
+			tokenVar = corev1.EnvVar{
+				Name: "K6_CLOUD_TOKEN",
+				ValueFrom: &corev1.EnvVarSource{
+					SecretKeyRef: &corev1.SecretKeySelector{
+						LocalObjectReference: corev1.LocalObjectReference{Name: tokenInfo.SecretName()},
+						Key:                  "token",
+					},
+				},
+			}
+		}
+
+		aggregationVars, err := cloud.DecodeAggregationConfig(k6.GetStatus().AggregationVars)
+		if err != nil {
+			return nil, err
+		}
+		env = append(env, aggregationVars...)
+		env = append(env, corev1.EnvVar{
+			Name:  "K6_CLOUD_PUSH_REF_ID",
+			Value: k6.GetStatus().TestRunID,
+		}, tokenVar)
+	}
+
+	env = append(env, k6.GetSpec().Runner.Env...)
+
 	if k6.GetSpec().Arguments != "" {
-		args := strings.Split(k6.GetSpec().Arguments, " ")
+		// Expand on environment variables using envmap from `env`
+		envStringSlice := convertEnvVarsToStringSlice(env)
+		envMap := interpolate.NewSliceEnv(envStringSlice)
+		expandedArgs, _ := interpolate.Interpolate(envMap, k6.GetSpec().Arguments)
+		args := strings.Split(expandedArgs, " ")
 		command = append(command, args...)
 	}
 
@@ -117,43 +160,43 @@ func NewRunnerJob(k6 *v1alpha1.TestRun, index int, tokenInfo *cloud.TokenInfo) (
 	ports := []corev1.ContainerPort{{ContainerPort: 6565}}
 	ports = append(ports, k6.GetSpec().Ports...)
 
-	env := newIstioEnvVar(k6.GetSpec().Scuttle, istioEnabled)
-
-	// this is a cloud test run
-	if len(k6.TestRunID()) > 0 {
-		// cloud output case
-		tokenVar := corev1.EnvVar{
-			Name:  "K6_CLOUD_TOKEN",
-			Value: tokenInfo.Value(),
-		}
-
-		if v1alpha1.IsTrue(k6, v1alpha1.CloudPLZTestRun) {
-			// temporary hack
-			k6.GetStatus().AggregationVars = "2|5s|3s|10s|10"
-			tokenVar = corev1.EnvVar{
-				Name: "K6_CLOUD_TOKEN",
-				ValueFrom: &corev1.EnvVarSource{
-					SecretKeyRef: &corev1.SecretKeySelector{
-						LocalObjectReference: corev1.LocalObjectReference{Name: tokenInfo.SecretName()},
-						Key:                  "token",
-					},
-				},
-			}
-		}
-
-		aggregationVars, err := cloud.DecodeAggregationConfig(k6.GetStatus().AggregationVars)
-		if err != nil {
-			return nil, err
-		}
-		env = append(env, aggregationVars...)
-		env = append(env, corev1.EnvVar{
-			Name:  "K6_CLOUD_PUSH_REF_ID",
-			Value: k6.GetStatus().TestRunID,
-		}, tokenVar)
-	}
-
-	env = append(env, k6.GetSpec().Runner.Env...)
-
+	// env := newIstioEnvVar(k6.GetSpec().Scuttle, istioEnabled)
+	//
+	// // this is a cloud test run
+	// if len(k6.TestRunID()) > 0 {
+	// 	// cloud output case
+	// 	tokenVar := corev1.EnvVar{
+	// 		Name:  "K6_CLOUD_TOKEN",
+	// 		Value: tokenInfo.Value(),
+	// 	}
+	//
+	// 	if v1alpha1.IsTrue(k6, v1alpha1.CloudPLZTestRun) {
+	// 		// temporary hack
+	// 		k6.GetStatus().AggregationVars = "2|5s|3s|10s|10"
+	// 		tokenVar = corev1.EnvVar{
+	// 			Name: "K6_CLOUD_TOKEN",
+	// 			ValueFrom: &corev1.EnvVarSource{
+	// 				SecretKeyRef: &corev1.SecretKeySelector{
+	// 					LocalObjectReference: corev1.LocalObjectReference{Name: tokenInfo.SecretName()},
+	// 					Key:                  "token",
+	// 				},
+	// 			},
+	// 		}
+	// 	}
+	//
+	// 	aggregationVars, err := cloud.DecodeAggregationConfig(k6.GetStatus().AggregationVars)
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+	// 	env = append(env, aggregationVars...)
+	// 	env = append(env, corev1.EnvVar{
+	// 		Name:  "K6_CLOUD_PUSH_REF_ID",
+	// 		Value: k6.GetStatus().TestRunID,
+	// 	}, tokenVar)
+	// }
+	//
+	// env = append(env, k6.GetSpec().Runner.Env...)
+	//
 	volumes := script.Volume()
 	volumes = append(volumes, k6.GetSpec().Runner.Volumes...)
 
