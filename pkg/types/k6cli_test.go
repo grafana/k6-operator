779313f569ab9b4199b620b42475321c270981c7
Olha Yevtushenko <yorugac@gmail.com>
2026-04-10 15:53:59 +0300

    fix: filter additional args from k6 CLI to initializer

--- pkg/types/k6cli_test.go
+++ pkg/types/k6cli_test.go
@@ -8,10 +8,10 @@ import (
 
 func Test_ParseCLI(t *testing.T) {
 	tests := []struct {
-		name           string
-		argLine        string
-		cli            CLI
-		validArguments bool
+		name             string
+		argLine          string
+		cli              CLI
+		invalidArguments bool
 	}{
 		{
 			"EmptyArgs",
@@ -88,10 +88,52 @@ func Test_ParseCLI(t *testing.T) {
 			false,
 		},
 		{
+			"OmitLogOutputInDiffOrder",
+			`--out cloud --log-output=loki=https://cloudlogs.k6.io/api/v1/push,label.lz=my-plz,label.test_run_id=1111,header.Authorization="Token $(K6_CLOUD_TOKEN)" --no-thresholds`,
+			CLI{
+				ArchiveArgs: "--no-thresholds",
+				HasCloudOut: true,
+			},
+			false,
+		},
+		{
 			"InvalidArguments",
 			`run this-argument-does-not-matter.js -o json`,
 			CLI{},
 			true,
+		},
+		{
+			"SkipBlockHostnamesEquals",
+			`--vus 10 --block-hostnames="google.com" --duration 5s`,
+			CLI{
+				ArchiveArgs: "--vus 10 --duration 5s",
+			},
+			false,
+		},
+		{
+			"SkipBlacklistIpEquals",
+			`--vus 10 --blacklist-ip="8.8.8.8/32" --duration 5s`,
+			CLI{
+				ArchiveArgs: "--vus 10 --duration 5s",
+			},
+			false,
+		},
+		{
+			"SkipUserAgentEquals",
+			`--vus 10 --user-agent="foo" --duration 5s`,
+			CLI{
+				ArchiveArgs: "--vus 10 --duration 5s",
+			},
+			false,
+		},
+		{
+			"IncludeSystemEnvVars",
+			`--out cloud --include-system-env-vars`,
+			CLI{
+				ArchiveArgs: "--include-system-env-vars",
+				HasCloudOut: true,
+			},
+			false,
 		},
 	}
 
@@ -100,8 +142,7 @@ func Test_ParseCLI(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			t.Parallel()
 			cli, err := ParseCLI(test.argLine)
-
-			assert.Equal(t, test.validArguments, err != nil)
+			assert.Equal(t, test.invalidArguments, err != nil)
 			assert.Equal(t, test.cli.ArchiveArgs, cli.ArchiveArgs)
 			assert.Equal(t, test.cli.HasCloudOut, cli.HasCloudOut)
 		})
