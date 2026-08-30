196f8c3945b5ae4f3154f98eb1dd2400e733810e
Arunesh Dwivedi <arunesh.devops@gmail.com>
2026-08-30 17:10:08 +0000

    fix(controller): return error on start job create failure
    Signed-off-by: Arunesh Dwivedi <arunesh.devops@gmail.com>

--- internal/controller/k6_start.go
+++ internal/controller/k6_start.go
@@ -121,7 +121,7 @@ func StartJobs(ctx context.Context, log logr.Logger, k6 *v1alpha1.TestRun, r *Te
 	created, err := createJobIfNotExists(ctx, r.Client, starter)
 	if err != nil {
 		log.Error(err, "Failed to launch k6 test starter")
-		return res, nil
+		return res, err
 	}
 
 	if created {
